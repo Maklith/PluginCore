4dd8601a7c571a42ee2aebd961b3a5230a5956f2
MakesYT <2696703792@qq.com>
2024-12-05 16:10:25 +0800

    *情景底层架构修改

---
+++ ICustomScenarioValueSerializer.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Core.SDKs.CustomScenario;
+
+public interface ICustomScenarioValueSerializer
+{
+    public string Serialize<T>(T value);
+    
+    public object Deserialize(ReadOnlySpan<byte> value);
+}
--- Kitopia.cs
+++ Kitopia.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text.Json.Serialization;
+using Core.SDKs.CustomScenario;
 
 namespace PluginCore;
 
@@ -10,5 +11,5 @@ public class Kitopia
     public static IToastService IToastService;
     public static Dictionary<string, string> _i18n;
     public static Dictionary<Type, Func<object, string>> ToolTipConverters;
-    public static Dictionary<Type, JsonConverter> JsonConverters;
+    public static Dictionary<Type, ICustomScenarioValueSerializer> JsonConverters;
 }
