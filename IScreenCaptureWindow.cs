2c232b9cebaeb90bae5629c1618e2662fd68ddf7
MakesYT <2696703792@qq.com>
2024-12-12 22:20:16 +0800

    新增 即时截图节点

--- IScreenCaptureWindow.cs
+++ IScreenCaptureWindow.cs
@@ -9,6 +9,7 @@ public interface IScreenCaptureWindow
 {
     public void CaptureScreen();
     public void RequestUserSelectScreenInfo(Action<ScreenCaptureInfo> action);
+    public void RequestUserSelectScreenBytes(Action<ScreenCaptureResult> action, Action cancel );
 
     public Task<ScreenCaptureInfo> GetScreenCaptureInfo();
 }
