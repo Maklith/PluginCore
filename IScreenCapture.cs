61e060bcc2e50dd3181b762ac8bbe0f1be71bba1
MakesYT <2696703792@qq.com>
2025-01-03 21:41:09 +0800

    新增 支持窗口截图

--- IScreenCapture.cs
+++ IScreenCapture.cs
@@ -1,27 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
+using Avalonia;
 using Avalonia.Media.Imaging;
 
 namespace PluginCore;
 
+public enum ScreenCaptureType
+{
+    屏幕,
+    窗口
+}
 public struct ScreenCaptureInfo()
 {
+    public ScreenCaptureType ScreenCaptureType = ScreenCaptureType.屏幕;
     public int X = -1;
     public int Y = -1;
     public int Width = -1;
     public int Height = -1;
     public ScreenInfo ScreenInfo;
+    public WindowInfo WindowInfo;
 }
 
 public struct ScreenInfo()
 {
     public IntPtr hMonitor;
-    public IntPtr hdcMonitor;
     public int X = -1;
     public int Y = -1;
     public int Width = -1;
     public int Height = -1;
+}
+
+public struct WindowInfo()
+{
+    public string Title;
+    public IntPtr Hwnd;
+    public Rect Rect;
 }
 public struct ScreenCaptureResult
 {
@@ -33,6 +47,7 @@ public struct ScreenCaptureResult
 public interface IScreenCapture
 {
     public List<ScreenCaptureInfo> GetAllScreenInfo();
+    public List<WindowInfo> GetAllWindowInfo();
     
     public ScreenCaptureInfo GetScreenCaptureInfoByIndex(int index);
     public Stack<ScreenCaptureResult> CaptureAllScreenBitmap();
--- IScreenCaptureManager.cs
+++ IScreenCaptureManager.cs
@@ -7,7 +7,7 @@ public interface IScreenCaptureManager
     public void SetCaptureMethodName(string methodName);
     public List<string> GetCaptureMethodName();
     public List<ScreenCaptureInfo> GetAllScreenInfo();
-    
+    public List<WindowInfo> GetAllWindowInfo();
     public ScreenCaptureInfo GetScreenCaptureInfoByIndex(int index);
     public Stack<ScreenCaptureResult> CaptureAllScreenBitmap();
     public Stack<ScreenCaptureResult> CaptureAllScreenBytes();
