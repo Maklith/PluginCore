9ae234ffa13235782477faba0e1f785f154b73c7
MakesYT <2696703792@qq.com>
2025-01-14 00:18:34 +0800

    新增 支持拓展截图功能

---
+++ Attribute/CaptureAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace PluginCore.Attribute;
+[AttributeUsage(AttributeTargets.Method)]
+public class CaptureAttribute : System.Attribute
+{
+    public string Description { get; set; }
+
+    public int Symbol { get; set; }
+
+    public CaptureAttribute( string description, int symbol = 0)
+    {
+        Description = description;
+        Symbol = symbol;
+    }
+}
