ca267f15c6d7d09c86a06f6fbd91bdf7ebc21859
MakesYT <2696703792@qq.com>
2025-02-15 16:53:05 +0800

    SDK更新 下放剪贴板接口 更新 支持识别二维码(截图,情景方法)

---
+++ IClipboardService.cs
@@ -0,0 +1,24 @@
+﻿#region
+
+using System.Threading.Tasks;
+using PluginCore;
+using Bitmap = Avalonia.Media.Imaging.Bitmap;
+
+#endregion
+
+namespace Core.SDKs.Services;
+
+public interface IClipboardService
+{
+    bool HasText();
+    string GetText();
+    bool SetText(string text);
+    bool HasImage();
+    Bitmap? GetImage();
+    bool SetImage(Bitmap image);
+    Task<bool> SetImageAsync(ScreenCaptureResult screenCaptureResult);
+}
+
+public enum ClipboardType
+{
+}
--- Kitopia.cs
+++ Kitopia.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text.Json.Serialization;
 using Core.SDKs.CustomScenario;
+using Core.SDKs.Services;
 using PluginCore.Onnx;
 
 namespace PluginCore;
@@ -9,6 +10,7 @@ namespace PluginCore;
 public class Kitopia
 {
     public static ISearchItemTool ISearchItemTool;
+    public static IClipboardService IClipboardService;
     public static IToastService IToastService;
     public static Dictionary<string, string> _i18n;
     public static Dictionary<Type, Func<object, string>> ToolTipConverters;
