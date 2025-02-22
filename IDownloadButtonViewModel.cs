cb30df151ae7b3039e291eb9b177067a02211956
MakesYT <2696703792@qq.com>
2025-02-22 22:10:35 +0800

    优化 Ocr效果 SDK更新 支持用户手动下载模型 KitopiaEx更新 新增服务器版文字识别模型

---
+++ IDownloadButtonViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Input;
+
+namespace PluginCore;
+
+public interface IDownloadButtonViewModel
+{
+    public bool NeedDownload { get;  }
+    public bool CanDownload { get;  }
+    public ICommand DownloadCommand { get; }
+    public bool IsIndeterminate { get;  }
+    public bool IsDownloading { get;  }
+    public double Progress { get;  }
+}
--- Onnx/OnnxModelInfo.cs
+++ Onnx/OnnxModelInfo.cs
@@ -1,11 +1,68 @@
-﻿namespace PluginCore.Onnx;
+﻿using System.IO;
+using System.Reflection;
+using System.Windows.Input;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace PluginCore.Onnx;
 
 
-public class OnnxModelInfo
+public partial class OnnxModelInfo :ObservableObject, IDownloadButtonViewModel
 {
+    private bool _needDownload;
+    private bool _canDownload;
+    private ICommand _downloadCommand;
+    private bool _isIndeterminate;
+    private bool _isDownloading;
+    private double _progress;
     public string Name { get; set; }
     public string Description { get; set; }
     
     public string SignName { get; set; }
     public string ModelPath { get; set; }
+
+    public OnnxModelInfo()
+    {
+        
+    }
+
+    public bool NeedDownload
+    {
+        get { return !File.Exists(ModelPath); }
+    }
+    public void NotifyNeedDownload()
+    {
+        OnPropertyChanged(nameof(NeedDownload));
+        OnPropertyChanged(nameof(ModelPath));
+    }
+
+
+    public bool CanDownload
+    {
+        get => _canDownload;
+        set => SetProperty(ref _canDownload,value);
+    }
+
+    public ICommand DownloadCommand
+    {
+        get => _downloadCommand;
+        set => SetProperty(ref _downloadCommand,value);
+    }
+
+    public bool IsIndeterminate
+    {
+        get => _isIndeterminate;
+        set =>  SetProperty(ref _isIndeterminate,value);
+    }
+
+    public bool IsDownloading
+    {
+        get => _isDownloading;
+        set =>  SetProperty(ref _isDownloading,value);
+    }
+
+    public double Progress
+    {
+        get => _progress;
+        set =>  SetProperty(ref _progress,value);
+    }
 }
