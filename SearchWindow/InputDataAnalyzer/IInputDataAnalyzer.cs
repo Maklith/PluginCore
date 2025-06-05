212480a31014fb283ef9776a131dd042c3fe32ae
MakesYT <2696703792@qq.com>
2025-06-05 13:31:06 +0800

    添加分析时间标志支持至各输入数据分析器

--- SearchWindow/InputDataAnalyzer/IInputDataAnalyzer.cs
+++ SearchWindow/InputDataAnalyzer/IInputDataAnalyzer.cs
@@ -1,9 +1,19 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Core.ViewModel;
 
 namespace PluginCore.SearchWindow.InputDataAnalyzer;
-
+[Flags]
+public enum IInputDataAnalyzeTimeFlags
+{
+    仅第一次打开时= 0,
+    仅有搜索内容打开时= 1,
+    搜索前= 2,
+    搜索时= 4,
+    
+}
 public interface IInputDataAnalyzer
 {
+    public IInputDataAnalyzeTimeFlags AnalyzeTimeFlags { get; }    
     public IEnumerable<SearchViewItem> AnalyzeInputData(IEnumerable<InputData> inputDatas);
 }
