b8619d155fd0017f420f4d9df0d1b942cf12a1a5
MakesYT <2696703792@qq.com>
2025-06-08 16:01:20 +0800

    优化 输入数据识别器以支持分析时间标志

--- SearchWindow/InputData/IInputDataIdentify.cs
+++ SearchWindow/InputData/IInputDataIdentify.cs
@@ -1,8 +1,9 @@
 ﻿using System.Collections.Generic;
+using PluginCore.SearchWindow.InputDataAnalyzer;
 
 namespace Core.ViewModel;
 
 public interface IInputDataIdentifier
 {
-    public IEnumerable<InputData> IdentifyInputData(string? s);
+    public IEnumerable<InputData> IdentifyInputData(IInputDataAnalyzeTimeFlags analyzeTimeFlags,string? s);
 }
