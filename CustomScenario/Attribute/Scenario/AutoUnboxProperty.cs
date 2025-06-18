b301130d8c517e2422293ac40c4a937082f09f64
MakesYT <2696703792@qq.com>
2025-06-18 15:39:06 +0800

    修复情景 支持自动解包属性和相关功能

---
+++ CustomScenario/Attribute/Scenario/AutoUnboxProperty.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace PluginCore.CustomScenario.Attribute.Scenario;
+[AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+public class AutoUnboxProperty : System.Attribute
+{
+    
+}
