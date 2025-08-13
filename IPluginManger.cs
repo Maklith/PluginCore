23842afce40c4f7ad221d3d33e5dae1b49efec19
liaom <2696703792@qq.com>
2025-08-13 16:53:49 +0800

    修复插件因情景导致无法卸载

--- IPluginManger.cs
+++ IPluginManger.cs
@@ -6,4 +6,5 @@ public interface IPluginManger
 {
     public Type GetType(string[] name);
     public PluginBaseInfo? GetPluginInfo(Type name);
+    public bool IsTypeFromThePlugin(Type type, string pluginName);
 }
