86279966e40b8beef1cf375dd854238f7dc94611
Maklith <2696703792@qq.com>
2025-10-30 09:11:15 +0800

    优化 现在消息提示修改到窗口内而不是windows自带toast

--- IToastService.cs
+++ IToastService.cs
@@ -1,12 +1,13 @@
-﻿
+﻿using Avalonia.Controls.Notifications;
 
 namespace PluginCore;
+
 /// <summary>
 /// 系统Toast服务
 /// </summary>
 public interface IToastService
 {
     public void Init();
-    public void Show(string header, string text);
+    public void Show(string header, string text, NotificationType notificationType = NotificationType.Information);
     public void Unregister();
 }
