1e92174ecac96bf846a4958881274c802fbeaf7e
Maklith <2696703792@qq.com>
2026-01-07 21:16:51 +0800

    优化 情景编辑中各种文本显示

--- CustomScenario/ToolTipConverter.cs
+++ CustomScenario/ToolTipConverter.cs
@@ -8,12 +8,12 @@ public class ToolTipConverter : IValueConverter
 {
     public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
-        if (value == null) return parameter;
+        if (value == null) return null;
         if (value is CustomScenarioValue valueTuple)
         {
             if (valueTuple.Value ==null)
             {
-                return parameter;
+                return null;
             }
             if (Kitopia.ToolTipConverters.ContainsKey(valueTuple.RealType))
                 return Kitopia.ToolTipConverters[valueTuple.RealType].Invoke(valueTuple.Value);
@@ -23,7 +23,16 @@ public class ToolTipConverter : IValueConverter
 
         if (Kitopia.ToolTipConverters.ContainsKey(value.GetType()))
             return (string)(Kitopia.ToolTipConverters[value.GetType()].Invoke(value));
-        else return value.ToString();
+        
+        else
+        {
+            var convert = value.ToString();
+            if (string.IsNullOrEmpty(convert))
+            {
+                return null;
+            }
+            return convert;
+        }
     }
 
     public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
