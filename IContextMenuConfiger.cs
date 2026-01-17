3b1f9607fe48db85fd4203849b338509448f03c5
maklith <2696703792@qq.com>
2026-01-17 22:16:29 +0800

    *重命名上下文菜单配置器为ExplorerContextMenuConfiger，并更新相关接口引用

---
+++ IContextMenuConfiger.cs
@@ -0,0 +1,21 @@
+// Author: liaom
+// SolutionName: Kitopia
+// ProjectName: Core
+// FileName:IContextMenuConfiger.cs
+// Date: 2026/01/17 21:01
+// FileEffect:
+
+using System.Collections.Generic;
+using Core.Services.Interfaces;
+
+namespace PluginCore;
+
+public interface IExplorerContextMenuConfiger
+{
+    public bool OverwriteMenuItems(List<ContextMenuItem> contextMenuItems);
+    public bool AddMenuItem(ContextMenuItem contextMenuItem);
+    public bool RemoveMenuItem(string title);
+    public bool RemoveMenuItem(ContextMenuItem contextMenuItem);
+    public List<ContextMenuItem> GetAllMenuItems();
+    
+}
