a4bb44e6684c8caf949c6e9d471b9419d58fafb7
Maklith <2696703792@qq.com>
2026-04-17 10:04:16 +0800

    优化代码结构，移除未使用的命名空间和引用，提升代码可读性

--- CustomScenario/CustomScenarioValue.cs
+++ CustomScenario/CustomScenarioValue.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Diagnostics;
 using System.Text.Json.Serialization;
 using CommunityToolkit.Mvvm.ComponentModel;
-using CommunityToolkit.Mvvm.Messaging;
 
 namespace PluginCore.CustomScenario;
 
--- IDeviceCommunication.cs
+++ IDeviceCommunication.cs
@@ -1,7 +1,2 @@
-﻿using System;
-using System.Collections.ObjectModel;
-using System.IO;
-using System.Threading.Tasks;
-
-namespace PluginCore;
+﻿namespace PluginCore;
 
